465e6688d3e99a59693fdcd5927e4cd2a3a3358a
K4ioSB <107511638+K4ioSB@users.noreply.github.com>
2022-10-15 19:56:59 -0300

    Update acesso.php

--- api/acesso.php
+++ api/acesso.php
@@ -3,9 +3,7 @@ $usuario = 'root';
 $senha = '';
 $database = 'login';
 $host = 'localhost';
-
 $mysqli = new mysqli($host,$usuario,$senha,$database);
-
 if($mysqli ->error){
     die("Falha ao conectar ao banco de dados: " .$mysqli->error);
 }
